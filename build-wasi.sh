e7ccd5e32baf353f0c2ec68fecd9a7e0f20fb044
Martin Evans <martindevans@gmail.com>
2026-04-01 16:44:23 +0100

    Made file executable

--- build-wasi.sh
+++ build-wasi.sh
@@ -106,7 +106,7 @@ popd > /dev/null
 
 # Attach a second memory to the file, currently unused
 PYTHON_WASM_FILE=($OUT_PATH/python3.*.wasm)
-pip install wabt
+chmod +x ./../wabt/bin/wasm2wat
 python3 ../add_memory.py $PYTHON_WASM_FILE $PYTHON_WASM_FILE asyncify_unwind_stack_memory_heap
 
 if [ $ASYNCIFY_OPTIMIZE -eq "1" ]
